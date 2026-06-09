ad51b295c47652abfb3a73495a19a454ed7681f9
hanzibo <2106611271@qq.com>
2026-06-09 21:40:12 +0800

    fix: defer category right-click menu to idle callback to prevent crash

--- clipboard_panel.py
+++ clipboard_panel.py
@@ -481,15 +481,19 @@ class ClipboardPanel(Gtk.Box):
         if cat is None:
             return False
 
-        menu = Gtk.Menu.new()
-        if cat.pinned:
-            item = Gtk.MenuItem.new_with_label("Remove from Top")
-        else:
-            item = Gtk.MenuItem.new_with_label("Show at Top")
-        item.connect("activate", lambda *_: self._toggle_pin(cat_id, not cat.pinned))
-        menu.append(item)
-        menu.show_all()
-        menu.popup_at_pointer(event)
+        def _show_menu():
+            menu = Gtk.Menu.new()
+            if cat.pinned:
+                item = Gtk.MenuItem.new_with_label("Remove from Top")
+            else:
+                item = Gtk.MenuItem.new_with_label("Show at Top")
+            item.connect("activate", lambda *_: self._toggle_pin(cat_id, not cat.pinned))
+            menu.append(item)
+            menu.show_all()
+            menu.popup_at_pointer(event)
+            return False
+
+        GLib.idle_add(_show_menu)
         return True
 
     def _toggle_pin(self, cat_id: str, pinned: bool):
